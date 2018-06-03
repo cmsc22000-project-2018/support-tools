f6872b8a3de0f6e9edd90c71147b5288a278410e
Richard Yuxiang Pei <richardpei@cs.uchicago.edu>
2018-06-03 01:12:12 -0500

    Updated testables using updated trie function names

--- src/testables.c
+++ src/testables.c
@@ -9,27 +9,27 @@ trie_t** return_trie_list() {
 }
 
 trie_t* first_trie() {
-  trie_t* first = new_trie('\0');
-  insert_string("and",first);
-  insert_string("would",first);
-  insert_string("wood",first);
+  trie_t* first = trie_new('\0');
+  trie_insert_string("and",first);
+  trie_insert_string("would",first);
+  trie_insert_string("wood",first);
   return first;
 }
 
 trie_t* second_trie() {
-  trie_t* second = new_trie('\0');
-  insert_string("test",second);
-  insert_string("something",second);
-  insert_string("wow",second);
-  insert_string("tea",second);
+  trie_t* second = trie_new('\0');
+  trie_insert_string("test",second);
+  trie_insert_string("something",second);
+  trie_insert_string("wow",second);
+  trie_insert_string("tea",second);
   return second;
 }
 trie_t* third_trie() {
-  trie_t* third = new_trie('\0');
-  insert_string("words",third);
-  insert_string("cat",third);
-  insert_string("catch",third);
-  insert_string("dog",third);
-  insert_string("borja",third);
+  trie_t* third = trie_new('\0');
+  trie_insert_string("words",third);
+  trie_insert_string("cat",third);
+  trie_insert_string("catch",third);
+  trie_insert_string("dog",third);
+  trie_insert_string("borja",third);
   return third;
 }
