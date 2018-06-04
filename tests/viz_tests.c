a0c0e86d59427bcc901d0256542762af4d64c5c7
Elizabeth Crowdus <ecrowdus@cs.uchicago.edu>
2018-06-04 00:23:19 -0500

    refactoring tests

--- tests/viz_tests.c
+++ tests/viz_tests.c
@@ -1,6 +1,8 @@
 #/*
  * Visualization unit tests 
  * Created by Elizabeth Crowdus and Richard Pei
+ *
+ * Uses API team's functions
  */
 
 #include <criterion/criterion.h>
@@ -11,172 +13,81 @@
 #include <stdlib.h>
 #include "../lib/api/include/trie.h"
 
-//helper make trie fn
+//helper make trie of words starting w 'a' fn
 trie_t* generate_a_trie(){
     trie_t *t = trie_new("test_a");
 
-    char* i1 = "a";
-    char* i2 = "at"; 
-    char* i3 = "and";
-
     cr_assert_eq(trie_insert(t, "a"), 0, "couldn't insert word into trie");
     cr_assert_eq(trie_insert(t, "at"), 0, "couldn't insert word into trie");
+    cr_assert_eq(trie_insert(t, "an"), 0, "couldn't insert word into trie");
     cr_assert_eq(trie_insert(t, "and"), 0, "couldn't insert word into trie");
     cr_assert_eq(trie_insert(t, "add"), 0, "couldn't insert word into trie");
     cr_assert_eq(trie_insert(t, "adding"), 0, "couldn't insert word into trie");
     cr_assert_eq(trie_insert(t, "ab"), 0, "couldn't insert word into trie");
     cr_assert_eq(trie_insert(t, "abstract"), 0, "couldn't insert word into trie");
-
 }
+
+//helper fn to compare char**
+//returns 1 if arrays are equal
+//returns 0 if arrays not equal
+int compare_char_arr(char** actual, int len_actual, char** expected, int len_expected){
+    //make sure lengths equal
+    if(len_actual != len_expected){
+        return 0;
+    }
+    //compare array contents
+    else{
+        //loop through arrays
+        int i;
+        for(i = 0; i < len_actual; i++){
+            //if words not the same
+            if(strcmp(actual[i], expected[i]) != 0){
+                return 0;
+            }
+        }
+    }
+}
+
 //print_only_words tests
-Test(print_only_words, true_ex1){
-    trie_t *t = trie_new("test");
+Test(print_only_words, true_null_prefix){
+    trie_t *t = generate_a_trie;
+    //actual
+    char** actual;
+    int* len_actual;
+    //assert that it works
+    cr_assert_eq(print_only_words(t, NULL, "", 0, actual, len_actual), 1, "error in print_only_words");
 
-    char* i1 = "a";
-    char* i2 = "at"; 
-    char* i3 = "and";
+    //expected
+    int len_expected = 8;
+    char *expected[len_expected];
+    expected[0]= "a";
+    expected[1] = "at";
+    expected[2] = "an";
+    expected[3] = "and";
+    expected[4] = "add";
+    expected [5] = "adding";
+    expected [6] = "ab";
+    expected [7] = "abstract";
 
-    cr_assert_eq(trie_insert(t, i1), 0, "couldn't insert word into trie");
-    cr_assert_eq(trie_insert(t, i2), 0, "couldn't insert word into trie");
-    cr_assert_eq(trie_insert(t, i3), 0, "couldn't insert word into trie");
-   
-/* eviz test cases */
+    //check that same char** produced
+    cr_assert_eq(compare_char_arr(actual, len_actual, expected, len_expected), 1); 
+} 
 
-Test(eviz, empty_trie_true){
-    char** input = [""];
-    trie_t *t = new_trie(input);
+Test(print_only_words, true_prefix){
+    trie_t *t = generate_a_trie;
+    //actual
+    char** actual;
+    int* len_actual;
+    //assert that it works
+    cr_assert_eq(print_only_words(t, "ad", "", 0, actual, len_actual), 1, "error in print_only_words");
 
-    int size = 0;
-    char** actual = eviz(t);
+    //expected
+    int len_expected = 2;
+    char *expected[len_expected];
+    expected[0]= "add";
+    expected[1] = "adding"
+    
+    //check that same char** produced
+    cr_assert_eq(compare_char_arr(actual, len_actual, expected, len_expected), 1); 
+}   
 
-    int result = compare_char_arr(actual, input, size);
-
-    cr_assert_eq(1, result, "eviz test failed");
-}
-
-Test(eviz, one_first_letter_true){
-    char** input = ["a", "an", "and", "at"];
-    trie_t *t = new_trie(input);
-
-    int size = 4;
-    char** actual = eviz(t);
-
-    int result = compare_char_arr(actual, input, size);
-
-    cr_assert_eq(1, result, "eviz test failed");
-}
-
-Test(eviz, one_first_letter_false){
-    char ** input = ['a', 'an', 'and', 'at'];
-    trie_t *t = new_trie(input);
-
-    int size = 4;
-    char** actual = eviz(t);
-    char** mod_input = ['a', 'an', 'and'];
-
-    int result = compare_char_arr(actual, mod_input, size);
-
-    cr_assert_eq(0, result, "eviz test failed");
-}
-
-/* LEAF VISUALIZATION TESTS
- * lviz()
- */
-
-/* lviz test cases */
-
-Test(lviz, one_first_letter_true){
-    char **input = ["c", "ca", "cat", "catc", "catch"];
-    trie_t *t = new_trie(input);
-
-    int size = 1;
-    char **expected = ["catch"];
-    char **actual = lviz(t);
-
-    int result = compare_char_arr(actual, expected, size);
-
-    cr_assert_eq(1, result, "lviz test failed");
-}
-
-Test(lviz, one_first_letter_false){
-    char **input = []"c", "ca", "cat", "catc", "catch"];
-    trie_t *t = new_trie(input);
-
-    int size = 1;
-    char **expected = ["catc"];
-    char **actual = lviz(t);
-
-    int result = compare_char_arr(actual, expected, size);
-
-    cr_assert_eq(0, result, "lviz test failed");
-}
-
-/* WORD VISUALIZATION TESTS
- * wviz()
- */
-
-
-/* wviz test cases */
-
-Test(wviz, one_first_letter_true){
-    char **input = ["c", "ca", "cat", "catc", "catch"];
-
-    trie_t *t = new_trie(input);
-
-    int size = 2;
-    char **expected = ["cat", "catch"];
-    char **actual = lviz(t);
-
-    int result = compare_char_arr(actual, expected, size);
-
-    cr_assert_eq(1, result, "wviz test failed");
-
-}
-
-Test(wviz, one_1st_letter_false){
-    char **input = ["c", "ca", "cat", "catc", "catch"];
-    trie_t *t = new_trie(input);
-
-    int size = 2;
-    char **expected = ["cat", "catc"];
-    char **actual = lviz(t);
-
-    int result = compare_char_arr(actual, expected, size);
-
-    cr_assert_eq(1, result, "wviz test failed");
-
-}
-
-/* SUBTREE VISUALIZATION TESTS
- * sviz()
- */
-
-/* sviz test cases */
-
-Test(sviz, working_trie_one_1st_letter){
-    char **input = ["c", "ca", "cat", "catc", "catch"];
-    trie_t *t = new_trie(input);
-
-    int size = 3;
-    char **expected = ["cat", "catc", "catch"];
-    char **actual = lviz(t, "cat");
-
-    int result = compare_char_arr(actual, expected, size);
-
-    cr_assert_eq(0, result, "wviz test failed");
-
-}
-
-Test(sviz, working_trie_full_alphabet_start_letters){
-    char **input = ["c", "ca", "cat", "catc", "catch"];
-    trie_t *t = new_trie(input);
-
-    int size = 3;
-    char **expected = ["cat", "ca", "catch"];
-    char **actual = lviz(t, "cat");
-
-    int result = compare_char_arr(actual, expected, size);
-
-    cr_assert_eq(0, result, "wviz test failed");
-
-}
